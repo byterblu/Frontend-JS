7d8104d5f2fef2da6693d7c36c46e67654355a7c
blublade <ynujose@gmail.com>
2022-08-24 10:04:47 -0400

    Drag and Drop

---
+++ Drag and Drop/index.js
@@ -0,0 +1,72 @@
+const dragElements = document.getElementById("dragElements")
+const dropZone = document.getElementById("dropZone")
+
+
+
+// dragElements.querySelectorAll("p")
+
+const draggableElements = dragElements.children
+
+let dragginElement 
+let dragginElementParent
+let OrdenadoDeElementos = dragElements.children
+
+for(let i = 0; i < draggableElements.length; i++){
+    const element = draggableElements[i];
+    element.draggable = true
+
+    element.ondrag = (event) =>{
+        dragginElement = event.target
+        if(dragginElement.parentNode===dragElements){
+            dragElements.removeChild(dragginElement)
+        }
+        
+    }
+    
+    // element.ondragend = (event) =>{
+       
+    // }
+
+    element.ondragend = (event) =>{
+        dragginElementParent = event.target
+        if(dragginElementParent.parentNode!==dropZone){
+            dragElements.appendChild(dragginElement)
+        } 
+
+    
+    }
+
+
+    
+    
+
+
+}
+
+dropZone.ondragover = (event) =>{
+    event.preventDefault()
+    dropZone.style.backgroundColor = "#ee8434"
+}
+
+dropZone.ondragleave = () =>{
+    dropZone.style.backgroundColor = "transparent"
+}
+
+dropZone.ondrop =(event) =>{
+    dropZone.appendChild(dragginElement)
+    dropZone.style.backgroundColor = "transparent"
+}
+
+dragElements.ondragover = (event) =>{
+    event.preventDefault()
+    dragElements.style.backgroundColor = "#717ec3"
+}
+
+dragElements.ondragleave = () =>{
+    dragElements.style.backgroundColor = "transparent"
+}
+
+dragElements.ondrop =(event) =>{
+    dragElements.appendChild(dragginElement)
+    dragElements.style.backgroundColor = "transparent"
+}
